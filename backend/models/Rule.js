a222042870ff8b320b14c76b2a06c73d585b09e8
Rupeshkumar2 <100811834+Rupeshkumar2@users.noreply.github.com>
2024-10-25 18:37:14 +0530

    Delete backend directory

--- backend/models/Rule.js
+++
@@ -1,9 +0,0 @@
-// backend/models/Rule.js
-const mongoose = require('mongoose');
-
-const ruleSchema = new mongoose.Schema({
-    ruleString: String,
-    createdAt: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model('Rule', ruleSchema);
